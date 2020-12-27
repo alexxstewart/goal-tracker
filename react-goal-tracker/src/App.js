c14e0198f9d43c73d3bc678b01ba28cf55cb87c2
Alex Stewart <alex.stewart14361@gmail.com>
2020-12-27 21:46:32 +1300

    Added clear button to remove all items. Items are now added and displayed on screen with skeleton buttons to remove and edit the individual goal

--- react-goal-tracker/src/App.js
+++ react-goal-tracker/src/App.js
@@ -1,28 +1,54 @@
 import './App.css';
 import React from 'react';
-import List from './List'
-import Alert from './Alert'
+import List from './List';
+import Alert from './Alert';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { uuid } from 'uuidv4';
 
 function App() {
 
-  const [buttonName, setButtonName] = React.useState('Submit')
-  const [goalList, setGoalList] = React.useState([])
+  const [buttonName, setButtonName] = React.useState('Submit');
+  const [inputGoal, setInputGoal] = React.useState('Enter Goal Here...');
+  const [goalList, setGoalList] = React.useState([]);
   const [alert, setAlert] = React.useState({
     msg: '',
     state: '',
     show: false
-  })
+  });
+
+  const goalChange = (e) => {
+    setInputGoal(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const buttonOnClickHandler = (e) => {
+
+    // create a goal with a unique ID
+    const newGoal = {
+      name: inputGoal,
+      id: uuid()
+    }
+
+    // add the new goal to the list of goals
+    setGoalList([...goalList, newGoal]);
+  }
+
+  const buttonHandler = (e) => {
+    console.log(e.target.value);
+  }
 
   return (
     <div className="center-div">
       <h1 id="app-title">Goal Tracker</h1>
       <Alert />
-      <form className="form-div">
-        <input id="input-box" type="text" defaultValue="Enter Goal Here..."/>
-        <button variant="primary" type="submit" class="btn btn-primary">{buttonName}</button>
-      </form>
+      <div className="form-div">
+        <input id="input-box" type="text" value={inputGoal} onChange={goalChange}/>
+        <button className="btn btn-primary" onClick={buttonOnClickHandler}>{buttonName}</button>
+      </div>
       <List list={goalList}/>
+      <div>
+        <button id="clear-button" className="btn btn-primary" onClick={() => setGoalList([])}>Clear All</button>
+      </div>
     </div>
   );
 }
--- react-goal-tracker/src/List.js
+++ react-goal-tracker/src/List.js
@@ -1,6 +1,15 @@
-function List(props) {
+function List({list}) {
   return (
     <div className="list-div">
+        {list.map((goal) => {
+            return (
+                <div className="goal-div" key={goal.id}>
+                    <p id="goal-text">{goal.name}</p>
+                    <button className="btn btn-primary" style={{margin: "10px"}}>Edit</button>
+                    <button className="btn btn-primary" style={{margin: "10px"}}>Remove</button>
+                </div>
+            )
+        })}
     </div>
   );
 }
